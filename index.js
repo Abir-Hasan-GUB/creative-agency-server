5208440c1effbc36a7576caa632507de8fae6829
Abir-Hasan-GUB <abirhasan6111@gmail.com>
2020-10-14 20:48:53 +0600

    Image Upload to mongoDB done

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const password = 'Volunteer-Network';
 const bodyParser = require('body-parser');
+const fs = require('fs-extra');
+const fileUpload = require('express-fileupload')
 const cors = require('cors');
 const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config();
@@ -9,6 +11,8 @@ const port = 5000;
 
 const app = express();
 app.use(cors());
+app.use(express.static('admin'));
+app.use(fileUpload());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -18,18 +22,57 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
     const collection = client.db("creative-agency").collection("agency's-information");
+    const adminCollection = client.db("creative-agency").collection("admin");
+    const courseCollection = client.db("creative-agency").collection("course");
 
     //Add Admin Mail
     app.post('/addAdmin', (req, res) => {
         const addAdminMail = req.body;
-        if(addAdminMail.email != ''){
-            collection.insertOne(addAdminMail)
-            .then(result => {
-              res.send(result.insertedCount > 0)
-            })
+        if (addAdminMail.email != '') {
+            adminCollection.insertOne(addAdminMail)
+                .then(result => {
+                    res.send(result.insertedCount > 0)
+                })
         }
-      })
+    })
 
+
+    //upload an image
+    app.post('/addSarvice', (req, res) => {
+        const file = req.files.file;
+        const name = req.body.name;
+        const designation = req.body.designation;
+        const filePath = `${__dirname}/admin/${file.name}`;
+        file.mv(filePath, err=>{
+            if(err){
+                console.log(err);
+                res.status(500).send({msg:'Faild to Upload IMG'});
+            }
+            const newImg = fs.readFileSync(filePath);
+            const encImg = newImg.toString('base64');
+
+            var image = {
+                contentType: req.files.file.mimetype,
+                size: req.files.file.size,
+                img: Buffer(encImg, 'base64')
+            };
+            courseCollection.insertOne({name,designation,image})
+            .then(result => {
+                fs.remove(filePath, error => {
+                    if(error){
+                        console.log(error);
+                        res.status(500).send({msg:'Faild to Upload IMG'});
+                    }
+                    res.send(result.insertedCount > 0)
+                })
+            })
+
+            // return res.send({name: file.name, path: `/${file.name}`})
+        })
+        console.log(name,file)
+    })
+
+    //Welcome Message
     app.get('/', (req, res) => {
         res.send("Abir Don't Worry We Working From MongoDB for You !!!");
     })
