bd3add340ace259415bb74b9eca340aec41aa81f
Abir-Hasan-GUB <abirhasan6111@gmail.com>
2020-10-14 13:32:38 +0600

    DB Connection Done

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const password = 'Volunteer-Network';
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
+require('dotenv').config()
+console.log(process.env.DB_USER)
+const port = 5000;
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//DB Connection
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.19f5u.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("db name").collection("collection name");
+
+
+})
+
+console.log("\nYEAA...Database Connected !!\n")
+//   client.close();
+
+app.listen(process.env.PORT || port);
